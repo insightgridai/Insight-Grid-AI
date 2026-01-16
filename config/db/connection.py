d95cb7e76f9b55fa858d2c079d22dbb1c13f1184
insightgridai <insightgridai@gmail.com>
2026-01-16 12:15:09 +0530

    Update connection.py

--- config/db/connection.py
+++ config/db/connection.py
@@ -1,16 +1,1 @@
-# db/connection.py
 
-import psycopg2
-from config.settings import Settings
-
-def get_db_connection():
-    Settings.validate()
-
-    return psycopg2.connect(
-        host=Settings.DB_HOST,
-        database=Settings.DB_NAME,
-        user=Settings.DB_USER,
-        password=Settings.DB_PASSWORD,
-        port=Settings.DB_PORT,
-        sslmode="require"
-    )
