19f4b0321fb87d4996225a56dc164bd59133911d
insightgridai <insightgridai@gmail.com>
2026-01-17 15:28:28 +0530

    Update credentials.py

--- config/credentials.py
+++ config/credentials.py
@@ -1,6 +1,6 @@
 import os
 
-os.environ["OPENAI_API_KEY"] ="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"
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
 DATABASE = {
     "HOST": "ep-divine-voice-a8sbaqui-pooler.eastus2.azure.neon.tech",
@@ -9,6 +9,7 @@ DATABASE = {
     "PASSWORD": "npg_0xPkYVANU1Mf",
     "PORT": 5432
 }
+
 
 
 
