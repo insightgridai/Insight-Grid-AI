8d26b5f322ce041482bd6cd8dbf4caacb4636cea
insightgridai <insightgridai@gmail.com>
2026-01-19 12:01:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -72,7 +72,7 @@ with db_col:
 
     if st.button("Test Database Connection"):
         try:
-            conn = get_db_connection()
+            conn = get_db_connection1()
             cur = conn.cursor()
             cur.execute("SELECT 1")
             cur.fetchone()
@@ -116,5 +116,6 @@ with agent_col:
                 except Exception as e:
                     st.error("Agent failed ❌")
                     st.exception(e)
+
 
 
