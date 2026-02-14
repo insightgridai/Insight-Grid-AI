f925b4bd751b71cd27c10cc668f47892ec6ce8df
insightgridai <insightgridai@gmail.com>
2026-02-14 16:49:30 +0530

    Update analyst_agent.py

--- agents/analyst_agent.py
+++ agents/analyst_agent.py
@@ -21,7 +21,7 @@ def get_analyst_app():
     Creates and returns the Analyst Agent workflow.
     """
 
-    llm = ChatOpenAI(model="gpt-5-nano")
+    llm = ChatOpenAI(model="gpt-5-mini")
 
     analyst_llm = llm.bind_tools([get_schema])
 
