bbe2fcae3283e71114224a1d36281858c4a7bbf9
insightgridai <insightgridai@gmail.com>
2026-03-25 23:26:26 +0530

    credentials.py

--- config/credentials.py
+++ config/credentials.py
@@ -3,10 +3,10 @@ import os
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
 DATABASE = {
-    "HOST": "ep-super-heart-a8d6wb15-pooler.eastus2.azure.neon.tech",
+    "HOST": "ep-divine-voice-a8sbaqui-pooler.eastus2.azure.neon.tech",
     "NAME": "azure",
     "USER": "neondb_owner",
-    "PASSWORD": "npg_rHCI4YmD9syR",
+    "PASSWORD": "npg_0xPkYVANU1Mf",
     "PORT": 5432
 }
 
