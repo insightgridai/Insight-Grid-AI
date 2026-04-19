7a65269dd3bf78adee4c79ba9be1cff126c0fe2d
insightgridai <insightgridai@gmail.com>
2026-04-19 23:51:13 +0530

    Update parser.py

--- utils/parser.py
+++ utils/parser.py
@@ -1,1 +1,13 @@
+# -----------------------------------------
+# Convert AI response string to JSON safely
+# -----------------------------------------
 
+import json
+
+def parse_response(response):
+    try:
+        start = response.find("{")
+        end = response.rfind("}") + 1
+        return json.loads(response[start:end])
+    except:
+        return None
